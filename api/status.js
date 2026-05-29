6eaf683ae9635c1e2d0f3e2ecaec56cb8ad7d14c
HR-XXII <betazona1984@gmail.com>
2026-05-30 00:25:16 +0900

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,21 @@
+export default async function handler(req, res) {
 
+const DID_API_KEY = "5WhdRllP9b6ZPrvysZfeg2w2MU4";
+process.env.DID_API_KEY;
+
+const response = await fetch(
+`https://api.d-id.com/talks/${req.query.id}`,
+{
+headers: {
+Authorization: `Bearer ${DID_API_KEY}`,
+Accept: "application/json"
+}
+}
+);
+
+const data =
+await response.json();
+
+res.status(200).json(data);
+
+}
